e820e2793ed51534df05f1d1d5e4973906625e6a
Larissa Coutinho <160404240+LarissaCoutinhoo@users.noreply.github.com>
2026-02-04 13:15:53 -0300

    feat: adiciona solução do exercício 7

--- src/ExercicioSete.java
+++ src/ExercicioSete.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class ExercicioSete {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         /* 7- Peça ao usuário para digitar vários números inteiros.
         O programa deve continuar pedindo números enquanto o valor digitado for diferente de zero.
         Ao final, mostre quantos números foram digitados (o zero não entra na contagem).*/
+
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        int contador = 0;
+
+        do{
+            System.out.println("Digite um número inteiro. (0 para sair)");
+            numero = scanner.nextInt();
+                if(numero != 0){
+                    contador++;
+                }
+        } while(numero != 0);
+
+        System.out.println("Você digitou " +contador+ " número(s).");
+        scanner.close();
     }
 }
