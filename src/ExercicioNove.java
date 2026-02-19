fcb24c351c292732fef50b106b0f306702226034
LarissaCoutinhoo <160404240+LarissaCoutinhoo@users.noreply.github.com>
2026-02-19 14:54:40 -0300

    feat: adiciona solução do exercício 9

--- src/ExercicioNove.java
+++ src/ExercicioNove.java
@@ -4,5 +4,18 @@ public class ExercicioNove {
     public static void main (String[] args){
         /* 9-Peça ao usuário um número inteiro.
             Mostre na tela a tabuada desse número, do 1 ao 10.*/
-    }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número inteiro");
+
+        int numero = scanner.nextInt();
+        int posicao;
+
+        for(posicao = 1; posicao <= 10; posicao++){
+            int resultado = numero * posicao;
+            System.out.println(numero+ " x " +posicao+ " = " +resultado);
+        }
+
+        scanner.close();
+        }
 }
